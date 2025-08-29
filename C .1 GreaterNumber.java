743b3577ea02594b0ed3448bc8af43535a88fc6c
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-08-29 14:51:14 +0530

    Update C .1 GreaterNumber.java

--- C .1 GreaterNumber.java
+++ C .1 GreaterNumber.java
@@ -3,9 +3,9 @@ class Greater
 {
 	public static void main(String abc[])
 	{
-	   int a=10;
-		  int b=40;
-	  	if(a>b)
+		int a=10;
+		int b=40;
+		if(a>b)
 		{
 		  System.out.println("a is greater");
 		}
