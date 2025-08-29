9230cd9b9346bc675d9e11c079b9d8d6346f3236
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-08-29 15:04:55 +0530

    Update C .6 LeapYear.java

--- C .6 LeapYear.java
+++ C .6 LeapYear.java
@@ -2,7 +2,7 @@
 public class LeapYearCheck {
     public static void main(String[] args) {
         int year = 2024;
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        if (year % 4 == 0){
           System.out.println(year + " is a Leap Year");
         } 
      else 
