ddc54e0d1e6f38c5d872393c2555c31fe28526ae
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-09-04 18:58:22 +0530

    Create E .4 Loop_Through_Array.java

---
+++ E .4 Loop_Through_Array.java
@@ -0,0 +1,14 @@
+package array.java;
+
+public class main 
+{
+ public static void main(String[] args) 
+ {
+	 String[] cars= {"volvo","BMW","Ford","Swift"};
+	 for(int i=0;i<cars.length;i++)
+	 {
+		 System.out.println(cars[i]);
+	 }
+	
+ } 
+}
