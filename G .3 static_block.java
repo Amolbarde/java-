c6bef9b4d4673f4df0542cc360682ca5164779a1
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-09-05 09:49:58 +0530

    Update G .3 static_block.java

--- G .3 static_block.java
+++ G .3 static_block.java
@@ -1,1 +1,14 @@
 
+package static_Keyowrd.java;
+public class static_block 
+{
+ static {
+	     System.out.println("Static block is running");
+      }
+ public static void main(String[] args)
+ {
+	System.out.println("main method is running");
+  }
+}
+//Static block is running
+//main method is running
