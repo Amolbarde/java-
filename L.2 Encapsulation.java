797aab6c2fb4ab1add7651b4a409cba1744c0070
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-10-11 18:53:25 +0530

    Create L.2 Encapsulation.java

---
+++ L.2 Encapsulation.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String nm;
+
+    public String getName() {
+        return nm;
+    }
+
+    public void setName(String name) {
+        this.nm = name;
+    }
+}
