14a45184932a03c10803ae3d7efd193399905f09
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-10-13 20:21:44 +0530

    Update L.1 Java Encapsulation.java

--- L.1 Java Encapsulation.java
+++ L.1 Java Encapsulation.java
@@ -1,1 +1,21 @@
 
+* Incapsulation:Encapsulation is the process of hiding the internal details of how an 
+  object works and only exposing what’s necessary through methods.
+
+* Think of a capsule pill — it hides the medicine inside. You just take the pill without
+   knowing what's exactly inside or how it's made.
+
+* Incapsulation doing using
+   . private field(hide the data)
+   . public getter/setter
+
+  * Rules/Steps to Implement Encapsulation:
+    .Make variables private
+    .Provide public getter/setter methods
+    .Keep logic inside setters/getters if needed (e.g., validation)
+
+*9. Benefits of Encapsulation:
+🔒 Data hiding
+🛠 Easy to change code internally without affecting outside code
+🚫 Prevents invalid data (via validation)
+🧹 Makes code cleaner and modular
