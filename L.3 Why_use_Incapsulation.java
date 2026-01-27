afbdaacc92f0744b202c6cd2270eb0aa5f0d4a8e
Amolbarde <amolb4055@gmail.com>
2026-01-27 21:12:25 +0530

    Replaced entire project with updated Java programs and cleaned old files

--- L.3 Why_use_Incapsulation.java
+++
@@ -1,9 +0,0 @@
-Why Use Encapsulation?
-_______________________________________________________________________|
-    Benefit       	  | Description                                    |
-_______________________________________________________________________|
-✅ Data Hiding       	| Prevent direct access to sensitive fields      |
-✅ Control Access     | Allow only valid values via setters           |
-✅ Code Flexibility	  | Change internal code without affecting users |
-✅ Reusability	      | Easier to maintain and extend                  |
-  _____________________________________________________________________|
--- README.md
+++
@@ -1,5 +0,0 @@
-# java-
-<hr>
-<h3>all java programs is here run by me</h3>
-<h5>Author- Amol barde</h5>
-<hr>
